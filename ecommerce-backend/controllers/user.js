f3b09eecf551513b0eab24819c3fce8c99f5ec96
Jaysonliu <leobest9688@gmail.com>
2020-04-25 21:59:09 +0800

    require signin middleware

--- ecommerce-backend/controllers/user.js
+++ ecommerce-backend/controllers/user.js
@@ -50,3 +50,8 @@ exports.signout = (req, res) => {
     res.clearCookie("t");
     res.json({ message: "Signout success" });
 };
+
+exports.requireSignin = expressJwt({
+    secret: process.env.JWT_SECRET,
+    userProperty: "auth"
+});
