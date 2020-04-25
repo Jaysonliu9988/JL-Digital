7b2d0ea40cdb391a04bf0876015163fde8eec4a4
Jaysonliu <leobest9688@gmail.com>
2020-04-25 15:43:05 +0800

    connecting to MongoDB

--- ecommerce-backend/app.js
+++ ecommerce-backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
 require('dotenv').config()
 
+// app
+const app = express()
+
+// db
+mongoose.connect(process.env.DATABASE || "database://localhost/ecommerce-backend", {
+    useNewUrlParser: true,
+    useCreateIndex: true
+}).then(() => console.log("DB Connected"));
+
+
+// routes
 app.get("/", (req, res) => {
     res.send("hello from node");
 });
@@ -11,3 +22,4 @@ const port = process.env.PORT || 8000
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
