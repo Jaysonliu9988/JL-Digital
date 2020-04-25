8c122083f2298f817cc40b5beed5768cd0c78a33
Jaysonliu <leobest9688@gmail.com>
2020-04-25 16:15:37 +0800

    virtual fields and methods

--- ecommerce-backend/models/user.js
+++ ecommerce-backend/models/user.js
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema(
             type: String,
             trim: true,
             required: true,
-            unique: 32
+            unique: true
         },
         hashed_password: {
             type: String,
@@ -36,3 +36,31 @@ const userSchema = new mongoose.Schema(
     },
     { timestamps: true }
 );
+
+// virtual field
+userSchema
+    .virtual("password")
+    .set(function(password) {
+        this._password = password;
+        this.salt = uuidv1();
+        this.hashed_password = this.encryptPassword(password);
+    })
+    .get(function() {
+        return this._password;
+    });
+
+userSchema.methods = {
+    encryptPassword: function(password) {
+        if (!password) return "";
+        try {
+            return crypto
+                .createHmac("sha1", this.salt)
+                .update(password)
+                .digest("hex");
+        } catch (err) {
+            return "";
+        }
+    }
+};
+
+module.exports = mongoose.model("User", userSchema);
