09aeca6fb3e165bb71ee3e48772733ab72913790
Jaysonliu <leobest9688@gmail.com>
2020-04-25 21:19:14 +0800

    friendly error message

--- ecommerce-backend/controllers/user.js
+++ ecommerce-backend/controllers/user.js
@@ -1,14 +1,17 @@
 const User = require("../models/user");
+const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.signup = (req, res) => {
-    console.log("req.body", req.body);
+    // console.log("req.body", req.body);
     const user = new User(req.body);
     user.save((err, user) => {
         if (err) {
             return res.status(400).json({
-                error
+                error: errorHandler(err)
             });
         }
+        user.salt = undefined;
+        user.hashed_password = undefined;
         res.json({
             user
         });
