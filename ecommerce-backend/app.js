fef71a79cfbacf63006038d5e7bed4a689ba2a5a
Jaysonliu <leobest9688@gmail.com>
2020-04-25 22:34:49 +0800

    creat category model route and controller

--- ecommerce-backend/app.js
+++ ecommerce-backend/app.js
@@ -8,6 +8,7 @@ require("dotenv").config();
 // import routes
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
 
 // app
 const app = express();
@@ -29,6 +30,7 @@ app.use(expressValidator());
 // routes middleware
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
 
 const port = process.env.PORT || 8000;
 
