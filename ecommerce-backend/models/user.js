ef5ada339dce7366ff11dfb23d77684c06e4028d
Jaysonliu <leobest9688@gmail.com>
2020-04-25 16:12:08 +0800

    creat user schema

---
+++ ecommerce-backend/models/user.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const uuidv1 = require("uuid/v1");
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: true,
+            maxlength: 32
+        },
+        email: {
+            type: String,
+            trim: true,
+            required: true,
+            unique: 32
+        },
+        hashed_password: {
+            type: String,
+            required: true
+        },
+        about: {
+            type: String,
+            trim: true
+        },
+        salt: String,
+        role: {
+            type: Number,
+            default: 0
+        },
+        history: {
+            type: Array,
+            default: []
+        }
+    },
+    { timestamps: true }
+);
