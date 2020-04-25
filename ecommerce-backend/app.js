d619a7321a956de5589cd4280157819b585891d7
Jaysonliu <leobest9688@gmail.com>
2020-04-25 21:32:25 +0800

    Using express validator

--- ecommerce-backend/app.js
+++ ecommerce-backend/app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
 require("dotenv").config();
 // import routes
 const userRoutes = require("./routes/user");
@@ -12,7 +13,7 @@ const app = express();
 
 // db
 mongoose
-    .connect(process.env.DATABASE || "mongodb://localhost/ecommerce-backend", {
+    .connect(process.env.DATABASE, {
         useNewUrlParser: true,
         useCreateIndex: true
     })
@@ -22,6 +23,7 @@ mongoose
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(expressValidator());
 
 // routes middleware
 app.use("/api", userRoutes);
