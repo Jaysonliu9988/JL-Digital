7ef03e9a09a0a178734318329577c3eed65469ce
Jaysonliu <leobest9688@gmail.com>
2020-04-26 21:33:42 +0800

    create product validation

--- ecommerce-backend/controllers/product.js
+++ ecommerce-backend/controllers/product.js
@@ -13,16 +13,48 @@ exports.create = (req, res) => {
                 error: "Image could not be uploaded"
             });
         }
+        // check for all fields
+        const {
+            name,
+            description,
+            price,
+            category,
+            quantity,
+            shipping
+        } = fields;
+
+        if (
+            !name ||
+            !description ||
+            !price ||
+            !category ||
+            !quantity ||
+            !shipping
+        ) {
+            return res.status(400).json({
+                error: "All fields are required"
+            });
+        }
+
         let product = new Product(fields);
 
+        // 1kb = 1000
+        // 1mb = 1000000
+
         if (files.photo) {
+            // console.log("FILES PHOTO: ", files.photo);
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: "Image should be less than 1mb in size"
+                });
+            }
             product.photo.data = fs.readFileSync(files.photo.path);
             product.photo.contentType = files.photo.type;
         }
 
         product.save((err, result) => {
             if (err) {
-                return res.staus(400).json({
+                return res.status(400).json({
                     error: errorHandler(err)
                 });
             }
