7d7056f315c2e92fb1412a8b7c7044e0127d75ea
Jaysonliu <leobest9688@gmail.com>
2020-04-26 22:18:19 +0800

    add category by id and single category

--- ecommerce-backend/controllers/category.js
+++ ecommerce-backend/controllers/category.js
@@ -1,6 +1,18 @@
 const Category = require("../models/category");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
+exports.categoryById = (req, res, next, id) => {
+    Category.findById(id).exec((err, category) => {
+        if (err || !category) {
+            return res.status(400).json({
+                error: "Category does not exist"
+            });
+        }
+        req.category = category;
+        next();
+    });
+};
+
 exports.create = (req, res) => {
     const category = new Category(req.body);
     category.save((err, data) => {
@@ -11,4 +23,8 @@ exports.create = (req, res) => {
         }
         res.json({ data });
     });
+};
+
+exports.read = (req, res) => {
+    return res.json(req.category);
 };
