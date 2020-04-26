4d35aeea9da1970e395259213969a14869d5a19c
Jaysonliu <leobest9688@gmail.com>
2020-04-26 21:29:41 +0800

    create product using postman

--- ecommerce-backend/app.js
+++ ecommerce-backend/app.js
@@ -9,6 +9,7 @@ require("dotenv").config();
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
 
 // app
 const app = express();
@@ -31,6 +32,7 @@ app.use(expressValidator());
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
 
 const port = process.env.PORT || 8000;
 
