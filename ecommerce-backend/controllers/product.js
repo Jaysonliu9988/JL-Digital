defbbdd0c0e0197949500807314d8e557ec22df4
Jaysonliu <leobest9688@gmail.com>
2020-04-26 21:45:53 +0800

    make product by id middleware and single product

--- ecommerce-backend/controllers/product.js
+++ ecommerce-backend/controllers/product.js
@@ -4,6 +4,23 @@ const fs = require("fs");
 const Product = require("../models/product");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
+exports.productById = (req, res, next, id) => {
+    Product.findById(id).exec((err, product) => {
+        if (err || !product) {
+            return res.status(400).json({
+                error: "Product not found"
+            });
+        }
+        req.product = product;
+        next();
+    });
+};
+
+exports.read = (req, res) => {
+    req.product.photo = undefined;
+    return res.json(req.product);
+};
+
 exports.create = (req, res) => {
     let form = new formidable.IncomingForm();
     form.keepExtensions = true;
--- ecommerce-backend/routes/product.js
+++ ecommerce-backend/routes/product.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { create } = require("../controllers/product");
+const { create, productById, read } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
-router.post("/product/create/:userId", requireSignin, isAdmin, isAdmin, create);
+router.get("/product/:productId", read);
+router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
 
 router.param("userId", userById);
+router.param("productId", productById);
 
 module.exports = router;
