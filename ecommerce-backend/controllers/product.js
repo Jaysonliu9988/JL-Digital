83b9723d573bcbbb65c7a2ad825c8dd0af5a7e42
Jaysonliu <leobest9688@gmail.com>
2020-04-26 22:38:47 +0800

    create product by sell and arrival on request query

--- ecommerce-backend/controllers/product.js
+++ ecommerce-backend/controllers/product.js
@@ -153,3 +153,30 @@ exports.update = (req, res) => {
         });
     });
 };
+
+/**
+ * sell / arrival
+ * by sell = /products?sortBy=sold&order=desc&limit=4
+ * by arrival = /products?sortBy=createdAt&order=desc&limit=4
+ * if no params are sent, then all products are returned
+ */
+
+exports.list = (req, res) => {
+    let order = req.query.order ? req.query.order : "asc";
+    let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
+    let limit = req.query.limit ? parseInt(req.query.limit) : 6;
+
+    Product.find()
+        .select("-photo")
+        .populate("category")
+        .sort([[sortBy, order]])
+        .limit(limit)
+        .exec((err, products) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "Products not found"
+                });
+            }
+            res.json(products);
+        });
+};
--- ecommerce-backend/routes/product.js
+++ ecommerce-backend/routes/product.js
@@ -6,7 +6,8 @@ const {
     productById,
     read,
     remove,
-    update
+    update,
+    list
 } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
@@ -27,6 +28,8 @@ router.put(
     isAdmin,
     update
 );
+
+router.get("/products", list);
 
 router.param("userId", userById);
 router.param("productId", productById);
