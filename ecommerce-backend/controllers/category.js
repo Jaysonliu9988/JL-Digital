f92784f21469261ddffbd523601a821835e2af24
Jaysonliu <leobest9688@gmail.com>
2020-04-26 22:32:17 +0800

    create category update delete and get all

--- ecommerce-backend/controllers/category.js
+++ ecommerce-backend/controllers/category.js
@@ -28,3 +28,41 @@ exports.create = (req, res) => {
 exports.read = (req, res) => {
     return res.json(req.category);
 };
+
+exports.update = (req, res) => {
+    const category = req.category;
+    category.name = req.body.name;
+    category.save((err, data) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json(data);
+    });
+};
+
+exports.remove = (req, res) => {
+    const category = req.category;
+    category.remove((err, data) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json({
+            message: "Category deleted"
+        });
+    });
+};
+
+exports.list = (req, res) => {
+    Category.find().exec((err, data) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json(data);
+    });
+};
