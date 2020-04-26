09036e2feff30b7fe2bea57b94a9066e31453345
Jaysonliu <leobest9688@gmail.com>
2020-04-27 00:09:11 +0800

    shared layout component

--- ecommerce-frontend/src/user/Signin.js
+++ ecommerce-frontend/src/user/Signin.js
@@ -1,5 +1,12 @@
 import React from "react";
+import Layout from "../core/Layout";
 
-const Signin = () => <div>signin</div>;
+const Signin = () => (
+    <Layout title="Signin" description="Signin to Node React E-commerce App">
+        ...
+    </Layout>
+);
 
 export default Signin;
+
+
--- ecommerce-frontend/src/user/Signup.js
+++ ecommerce-frontend/src/user/Signup.js
@@ -1,5 +1,10 @@
 import React from "react";
+import Layout from "../core/Layout";
 
-const Signup = () => <div>signup</div>;
+const Signup = () => (
+    <Layout title="Signup" description="Signup to Node React E-commerce App">
+        ...
+    </Layout>
+);
 
 export default Signup;
