2e9d72659a96b47bac0720f882f27e169ae0427d
Jaysonliu <leobest9688@gmail.com>
2020-04-26 23:19:35 +0800

    add the method for user profile read and update

--- ecommerce-backend/controllers/user.js
+++ ecommerce-backend/controllers/user.js
@@ -11,3 +11,27 @@ exports.userById = (req, res, next, id) => {
         next();
     });
 };
+
+exports.read = (req, res) => {
+    req.profile.hashed_password = undefined;
+    req.profile.salt = undefined;
+    return res.json(req.profile);
+};
+
+exports.update = (req, res) => {
+    User.findOneAndUpdate(
+        { _id: req.profile._id },
+        { $set: req.body },
+        { new: true },
+        (err, user) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "You are not authorized to perform this action"
+                });
+            }
+            user.hashed_password = undefined;
+            user.salt = undefined;
+            res.json(user);
+        }
+    );
+};
