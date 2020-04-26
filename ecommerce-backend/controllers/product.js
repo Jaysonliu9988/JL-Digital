7624b4b0ae94d263311f6c04246b36016da696f5
Jaysonliu <leobest9688@gmail.com>
2020-04-26 21:52:20 +0800

    add product delete

--- ecommerce-backend/controllers/product.js
+++ ecommerce-backend/controllers/product.js
@@ -79,3 +79,17 @@ exports.create = (req, res) => {
         });
     });
 };
+
+exports.remove = (req, res) => {
+    let product = req.product;
+    product.remove((err, deletedProduct) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json({
+            message: "Product deleted successfully"
+        });
+    });
+};
--- ecommerce-backend/routes/product.js
+++ ecommerce-backend/routes/product.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const { create, productById, read } = require("../controllers/product");
+const { create, productById, read, remove } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
 router.get("/product/:productId", read);
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
+router.delete(
+    "/product/:productId/:userId",
+    requireSignin,
+    isAuth,
+    isAdmin,
+    remove
+);
 
 router.param("userId", userById);
 router.param("productId", productById);
