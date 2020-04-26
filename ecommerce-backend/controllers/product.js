872ffac87e6940ab21d4a71fb56c553d9a08be06
Jaysonliu <leobest9688@gmail.com>
2020-04-26 23:01:12 +0800

    list product categories

--- ecommerce-backend/controllers/product.js
+++ ecommerce-backend/controllers/product.js
@@ -201,3 +201,14 @@ exports.listRelated = (req, res) => {
             res.json(products);
         });
 };
+
+exports.listCategories = (req, res) => {
+    Product.distinct("category", {}, (err, categories) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Categories not found"
+            });
+        }
+        res.json(categories);
+    });
+};
--- ecommerce-backend/routes/product.js
+++ ecommerce-backend/routes/product.js
@@ -8,7 +8,8 @@ const {
     remove,
     update,
     list,
-    listRelated
+    listRelated,
+    listCategories
 } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
@@ -32,6 +33,7 @@ router.put(
 
 router.get("/products", list);
 router.get("/products/related/:productId", listRelated);
+router.get("/products/categories", listCategories);
 
 router.param("userId", userById);
 router.param("productId", productById);
