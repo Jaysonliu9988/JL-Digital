45198cfeb5c4e872e18623790d1fef4d0c4e9e1f
Jaysonliu <leobest9688@gmail.com>
2020-04-26 22:02:57 +0800

    add product update

--- ecommerce-backend/controllers/product.js
+++ ecommerce-backend/controllers/product.js
@@ -93,3 +93,63 @@ exports.remove = (req, res) => {
         });
     });
 };
+
+exports.update = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Image could not be uploaded"
+            });
+        }
+        // check for all fields
+        const {
+            name,
+            description,
+            price,
+            category,
+            quantity,
+            shipping
+        } = fields;
+
+        if (
+            !name ||
+            !description ||
+            !price ||
+            !category ||
+            !quantity ||
+            !shipping
+        ) {
+            return res.status(400).json({
+                error: "All fields are required"
+            });
+        }
+
+        let product = req.product;
+        product = _.extend(product, fields);
+
+        // 1kb = 1000
+        // 1mb = 1000000
+
+        if (files.photo) {
+            // console.log("FILES PHOTO: ", files.photo);
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: "Image should be less than 1mb in size"
+                });
+            }
+            product.photo.data = fs.readFileSync(files.photo.path);
+            product.photo.contentType = files.photo.type;
+        }
+
+        product.save((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+            res.json(result);
+        });
+    });
+};
--- ecommerce-backend/routes/product.js
+++ ecommerce-backend/routes/product.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { create, productById, read, remove } = require("../controllers/product");
+const {
+    create,
+    productById,
+    read,
+    remove,
+    update
+} = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
@@ -13,6 +19,13 @@ router.delete(
     isAuth,
     isAdmin,
     remove
+);
+router.put(
+    "/product/:productId/:userId",
+    requireSignin,
+    isAuth,
+    isAdmin,
+    update
 );
 
 router.param("userId", userById);
