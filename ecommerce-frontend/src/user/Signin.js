c527402e6553b25993818a2946c04ee8a443c69e
Jaysonliu <leobest9688@gmail.com>
2020-04-27 15:27:41 +0800

    create user signin

--- ecommerce-frontend/src/user/Signin.js
+++ ecommerce-frontend/src/user/Signin.js
@@ -1,12 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
 import Layout from "../core/Layout";
+import { signin } from "../auth";
 
-const Signin = () => (
-    <Layout title="Signin" description="Signin to Node React E-commerce App">
-        ...
-    </Layout>
-);
+const Signin = () => {
+    const [values, setValues] = useState({
+        email: "",
+        password: "",
+        error: "",
+        loading: false,
+        redirectToReferrer: false
+    });
+
+    const { email, password, loading, error, redirectToReferrer } = values;
+
+    const handleChange = name => event => {
+        setValues({ ...values, error: false, [name]: event.target.value });
+    };
+
+    const clickSubmit = event => {
+        event.preventDefault();
+        setValues({ ...values, error: false, loading: true });
+        signin({ email, password }).then(data => {
+            if (data.error) {
+                setValues({ ...values, error: data.error, loading: false });
+            } else {
+                setValues({
+                    ...values,
+                    redirectToReferrer: true
+                });
+            }
+        });
+    };
+
+    const signUpForm = () => (
+        <form>
+            <div className="form-group">
+                <label className="text-muted">Email</label>
+                <input
+                    onChange={handleChange("email")}
+                    type="email"
+                    className="form-control"
+                    value={email}
+                />
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Password</label>
+                <input
+                    onChange={handleChange("password")}
+                    type="password"
+                    className="form-control"
+                    value={password}
+                />
+            </div>
+            <button onClick={clickSubmit} className="btn btn-primary">
+                Submit
+            </button>
+        </form>
+    );
+
+    const showError = () => (
+        <div
+            className="alert alert-danger"
+            style={{ display: error ? "" : "none" }}
+        >
+            {error}
+        </div>
+    );
+
+    const showLoading = () =>
+        loading && (
+            <div className="alert alert-info">
+                <h2>Loading...</h2>
+            </div>
+        );
+
+    const redirectUser = () => {
+        if (redirectToReferrer) {
+            return <Redirect to="/" />;
+        }
+    };
+
+    return (
+        <Layout
+            title="Signup"
+            description="Signup to Node React E-commerce App"
+            className="container col-md-8 offset-md-2"
+        >
+            {showLoading()}
+            {showError()}
+            {signUpForm()}
+            {redirectUser()}
+        </Layout>
+    );
+};
 
 export default Signin;
-
-
