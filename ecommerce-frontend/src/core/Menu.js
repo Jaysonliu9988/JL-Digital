de76cbd996112f3cc8ed43c72f47894af6c2920e
Jaysonliu <leobest9688@gmail.com>
2020-04-27 15:31:49 +0800

    set user signout

--- ecommerce-frontend/src/core/Menu.js
+++ ecommerce-frontend/src/core/Menu.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
+import { signout } from "../auth";
 
 const isActive = (history, path) => {
     if (history.location.pathname === path) {
@@ -40,6 +41,20 @@ const Menu = ({ history }) => (
                 >
                     Signup
                 </Link>
+            </li>
+
+            <li className="nav-item">
+                <span
+                    className="nav-link"
+                    style={{ cursor: "pointer", color: "#ffffff" }}
+                    onClick={() =>
+                        signout(() => {
+                            history.push("/");
+                        })
+                    }
+                >
+                    Signout
+                </span>
             </li>
         </ul>
     </div>
