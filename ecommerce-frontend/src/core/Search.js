1ee44136ff9fffd2a05b071903543fa9a8027cbd
Jaysonliu <leobest9688@gmail.com>
2020-04-27 18:51:50 +0800

    add search component

---
+++ ecommerce-frontend/src/core/Search.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from "react";
+import { getCategories } from "./apiCore";
+import Card from "./Card";
+
+const Search = () => {
+    const [data, setData] = useState({
+        categories: [],
+        category: "",
+        search: "",
+        results: [],
+        searched: false
+    });
+
+    const { categories, category, search, results, searched } = data;
+
+    const loadCategories = () => {
+        getCategories().then(data => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                setData({ ...data, categories: data });
+            }
+        });
+    };
+
+    useEffect(() => {
+        loadCategories();
+    }, []);
+
+    const searchSubmit = () => {
+        //
+    };
+
+    const handleChange = () => {
+        //
+    };
+
+    const searchForm = () => (
+        <form onSubmit={searchSubmit}>
+            <span className="input-group-text">
+                <div className="input-group input-group-lg">
+                    <div className="input-group-prepend">
+                        <select
+                            className="btn mr-2"
+                            onChange={handleChange("category")}
+                        >
+                            <option value="All">Pick Category</option>
+                            {categories.map((c, i) => (
+                                <option key={i} value={c._id}>
+                                    {c.name}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+
+                    <input
+                        type="search"
+                        className="form-control"
+                        onChange={handleChange("search")}
+                        placeholder="Search by name"
+                    />
+                </div>
+                <div
+                    className="btn input-group-append"
+                    style={{ border: "none" }}
+                >
+                    <button className="input-group-text">Search</button>
+                </div>
+            </span>
+        </form>
+    );
+
+    return (
+        <div className="row">
+            <div className="container mb-3">{searchForm()}</div>
+        </div>
+    );
+};
+
+export default Search;
