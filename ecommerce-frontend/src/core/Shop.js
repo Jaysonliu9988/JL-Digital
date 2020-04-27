e7d9ef1b3686b56deda8b1bdcfe9d9b040c74c91
Jaysonliu <leobest9688@gmail.com>
2020-04-27 17:49:44 +0800

    get categories in shop page

--- ecommerce-frontend/src/core/Shop.js
+++ ecommerce-frontend/src/core/Shop.js
@@ -1,8 +1,26 @@
 import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
 import Card from "./Card";
+import { getCategories } from "./apiCore";
 
 const Shop = () => {
+    const [categories, setCategories] = useState([]);
+    const [error, setError] = useState(false);
+
+    const init = () => {
+        getCategories().then(data => {
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setCategories(data);
+            }
+        });
+    };
+
+    useEffect(() => {
+        init();
+    }, []);
+
     return (
         <Layout
             title="Shop Page"
@@ -10,7 +28,7 @@ const Shop = () => {
             className="container-fluid"
         >
             <div className="row">
-                <div className="col-4">left sidebar</div>
+                <div className="col-4">{JSON.stringify(categories)}</div>
 
                 <div className="col-8">right</div>
             </div>
