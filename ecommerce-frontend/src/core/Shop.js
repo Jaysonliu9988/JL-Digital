72547c7b0bdb857981d6f4dc430264867a92be44
Jaysonliu <leobest9688@gmail.com>
2020-04-27 18:06:06 +0800

    show products by filter on shop page

--- ecommerce-frontend/src/core/Shop.js
+++ ecommerce-frontend/src/core/Shop.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
 import Card from "./Card";
-import { getCategories } from "./apiCore";
+import { getCategories, getFilteredProducts } from "./apiCore";
 import Checkbox from "./Checkbox";
 import RadioBox from "./RadioBox";
 import { prices } from "./fixedPrices";
@@ -12,6 +12,9 @@ const Shop = () => {
     });
     const [categories, setCategories] = useState([]);
     const [error, setError] = useState(false);
+    const [limit, setLimit] = useState(6);
+    const [skip, setSkip] = useState(0);
+    const [filteredResults, setFilteredResults] = useState([]);
 
     const init = () => {
         getCategories().then(data => {
@@ -23,8 +26,20 @@ const Shop = () => {
         });
     };
 
+    const loadFilteredResults = newFilters => {
+        // console.log(newFilters);
+        getFilteredProducts(skip, limit, newFilters).then(data => {
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setFilteredResults(data);
+            }
+        });
+    };
+
     useEffect(() => {
         init();
+        loadFilteredResults(skip, limit, myFilters.filters);
     }, []);
 
     const handleFilters = (filters, filterBy) => {
@@ -36,7 +51,7 @@ const Shop = () => {
             let priceValues = handlePrice(filters);
             newFilters.filters[filterBy] = priceValues;
         }
-
+        loadFilteredResults(myFilters.filters);
         setMyFilters(newFilters);
     };
 
@@ -81,7 +96,7 @@ const Shop = () => {
                     </div>
                 </div>
 
-                <div className="col-8">{JSON.stringify(myFilters)}</div>
+                <div className="col-8">{JSON.stringify(filteredResults)}</div>
             </div>
         </Layout>
     );
