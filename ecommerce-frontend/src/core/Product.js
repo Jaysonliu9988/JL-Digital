7da1f45677742fa957baafebb32629815e7924d6
Jaysonliu <leobest9688@gmail.com>
2020-04-27 19:26:34 +0800

    show related products

--- ecommerce-frontend/src/core/Product.js
+++ ecommerce-frontend/src/core/Product.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
-import { read } from "./apiCore";
+import { read, listRelated } from "./apiCore";
 import Card from "./Card";
 
 const Product = props => {
     const [product, setProduct] = useState({});
+    const [relatedProduct, setRelatedProduct] = useState([]);
     const [error, setError] = useState(false);
 
     const loadSingleProduct = productId => {
@@ -13,6 +14,14 @@ const Product = props => {
                 setError(data.error);
             } else {
                 setProduct(data);
+                // fetch related products
+                listRelated(data._id).then(data => {
+                    if (data.error) {
+                        setError(data.error);
+                    } else {
+                        setRelatedProduct(data);
+                    }
+                });
             }
         });
     };
@@ -20,7 +29,7 @@ const Product = props => {
     useEffect(() => {
         const productId = props.match.params.productId;
         loadSingleProduct(productId);
-    }, []);
+    }, [props]);
 
     return (
         <Layout
@@ -33,9 +42,20 @@ const Product = props => {
             className="container-fluid"
         >
             <div className="row">
-                {product && product.description && (
-                    <Card product={product} showViewProductButton={false} />
-                )}
+                <div className="col-8">
+                    {product && product.description && (
+                        <Card product={product} showViewProductButton={false} />
+                    )}
+                </div>
+
+                <div className="col-4">
+                    <h4>Related products</h4>
+                    {relatedProduct.map((p, i) => (
+                        <div className="mb-3">
+                            <Card key={i} product={p} />
+                        </div>
+                    ))}
+                </div>
             </div>
         </Layout>
     );
--- ecommerce-frontend/src/core/apiCore.js
+++ ecommerce-frontend/src/core/apiCore.js
@@ -64,3 +64,13 @@ export const read = productId => {
         })
         .catch(err => console.log(err));
 };
+
+export const listRelated = productId => {
+    return fetch(`${API}/products/related/${productId}`, {
+        method: "GET"
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
