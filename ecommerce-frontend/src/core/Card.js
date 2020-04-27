9febecac7408981261d92da97fd046acc1399c4f
Jaysonliu <leobest9688@gmail.com>
2020-04-27 17:45:24 +0800

    styling buttons and jumbotron

--- ecommerce-frontend/src/core/Card.js
+++ ecommerce-frontend/src/core/Card.js
@@ -12,7 +12,7 @@ const Card = ({ product }) => {
                     <p>{product.description}</p>
                     <p>${product.price}</p>
                     <Link to="/">
-                        <button className="btn btn-outline-primary mt-2 mb-2">
+                        <button className="btn btn-outline-primary mt-2 mb-2 mr-2">
                             View Product
                         </button>
                     </Link>
