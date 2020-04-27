7f7808a47c0a9acff785cd18a5ac2197bee74dcd
Jaysonliu <leobest9688@gmail.com>
2020-04-27 19:20:01 +0800

    Backend implementing of product detail on single product page

--- ecommerce-backend/controllers/product.js
+++ ecommerce-backend/controllers/product.js
@@ -5,15 +5,17 @@ const Product = require("../models/product");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.productById = (req, res, next, id) => {
-    Product.findById(id).exec((err, product) => {
-        if (err || !product) {
-            return res.status(400).json({
-                error: "Product not found"
-            });
-        }
-        req.product = product;
-        next();
-    });
+    Product.findById(id)
+        .populate("category")
+        .exec((err, product) => {
+            if (err || !product) {
+                return res.status(400).json({
+                    error: "Product not found"
+                });
+            }
+            req.product = product;
+            next();
+        });
 };
 
 exports.read = (req, res) => {
