a5b6fbc424c0cfbf2cae8e78199341c8318eaaf1
Jaysonliu <leobest9688@gmail.com>
2020-04-27 18:09:16 +0800

    load more button

--- ecommerce-frontend/src/core/Shop.js
+++ ecommerce-frontend/src/core/Shop.js
@@ -14,6 +14,7 @@ const Shop = () => {
     const [error, setError] = useState(false);
     const [limit, setLimit] = useState(6);
     const [skip, setSkip] = useState(0);
+    const [size, setSize] = useState(0);
     const [filteredResults, setFilteredResults] = useState([]);
 
     const init = () => {
@@ -33,8 +34,35 @@ const Shop = () => {
                 setError(data.error);
             } else {
                 setFilteredResults(data.data);
+                setSize(data.size);
+                setSkip(0);
             }
         });
+    };
+
+    const loadMore = () => {
+        let toSkip = skip + limit;
+        // console.log(newFilters);
+        getFilteredProducts(toSkip, limit, myFilters.filters).then(data => {
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setFilteredResults([...filteredResults, ...data.data]);
+                setSize(data.size);
+                setSkip(toSkip);
+            }
+        });
+    };
+
+    const loadMoreButton = () => {
+        return (
+            size > 0 &&
+            size >= limit && (
+                <button onClick={loadMore} className="btn btn-warning mb-5">
+                    Load more
+                </button>
+            )
+        );
     };
 
     useEffect(() => {
@@ -103,6 +131,8 @@ const Shop = () => {
                             <Card key={i} product={product} />
                         ))}
                     </div>
+                    <hr />
+                    {loadMoreButton()}
                 </div>
             </div>
         </Layout>
