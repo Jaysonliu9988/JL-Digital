8ed9266c16c346112218749df877176eb69db816
Jaysonliu <leobest9688@gmail.com>
2020-04-27 17:42:03 +0800

    show product image

--- ecommerce-frontend/src/core/Card.js
+++ ecommerce-frontend/src/core/Card.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ShowImage from "./ShowImage";
 
 const Card = ({ product }) => {
     return (
@@ -7,6 +8,7 @@ const Card = ({ product }) => {
             <div className="card">
                 <div className="card-header">{product.name}</div>
                 <div className="card-body">
+                    <ShowImage item={product} url="product" />
                     <p>{product.description}</p>
                     <p>${product.price}</p>
                     <Link to="/">
