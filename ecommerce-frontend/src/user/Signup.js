863a385022d513a60cfd163c496d397a74a4a29e
Jaysonliu <leobest9688@gmail.com>
2020-04-27 15:16:39 +0800

    add signup form handle change

--- ecommerce-frontend/src/user/Signup.js
+++ ecommerce-frontend/src/user/Signup.js
@@ -1,11 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../core/Layout";
 import { API } from "../config";
 
-const Signup = () => (
-    <Layout title="Signup" description="Signup to Node React E-commerce App">
-        { API }
-    </Layout>
-);
+const Signup = () => {
+    const [values, setValues] = useState({
+        name: "",
+        email: "",
+        password: "",
+        error: "",
+        success: false
+    });
+
+    const handleChange = name => event => {
+        setValues({ ...values, error: false, [name]: event.target.value });
+    };
+
+    const signUpForm = () => (
+        <form>
+            <div className="form-group">
+                <label className="text-muted">Name</label>
+                <input
+                    onChange={handleChange("name")}
+                    type="text"
+                    className="form-control"
+                />
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Email</label>
+                <input
+                    onChange={handleChange("email")}
+                    type="email"
+                    className="form-control"
+                />
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Password</label>
+                <input
+                    onChange={handleChange("password")}
+                    type="password"
+                    className="form-control"
+                />
+            </div>
+            <button className="btn btn-primary">Submit</button>
+        </form>
+    );
+
+    return (
+        <Layout
+            title="Signup"
+            description="Signup to Node React E-commerce App"
+            className="container col-md-8 offset-md-2"
+        >
+            {signUpForm()}
+            {JSON.stringify(values)}
+        </Layout>
+    );
+};
 
 export default Signup;
