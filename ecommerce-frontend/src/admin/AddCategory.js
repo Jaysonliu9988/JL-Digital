cb6492d52c636ff66d9b712f7a9dfa4de31026ca
Jaysonliu <leobest9688@gmail.com>
2020-04-27 16:07:50 +0800

    category create success and error

--- ecommerce-frontend/src/admin/AddCategory.js
+++ ecommerce-frontend/src/admin/AddCategory.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
+import { createCategory } from "./apiAdmin";
 
 const AddCategory = () => {
     const [name, setName] = useState("");
@@ -21,6 +22,14 @@ const AddCategory = () => {
         setError("");
         setSuccess(false);
         // make request to api to create category
+        createCategory(user._id, token, { name }).then(data => {
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setError("");
+                setSuccess(true);
+            }
+        });
     };
 
     const newCategoryFom = () => (
@@ -33,19 +42,45 @@ const AddCategory = () => {
                     onChange={handleChange}
                     value={name}
                     autoFocus
+                    required
                 />
             </div>
             <button className="btn btn-outline-primary">Create Category</button>
         </form>
     );
 
+    const showSuccess = () => {
+        if (success) {
+            return <h3 className="text-success">{name} is created</h3>;
+        }
+    };
+
+    const showError = () => {
+        if (error) {
+            return <h3 className="text-danger">Category should be unique</h3>;
+        }
+    };
+
+    const goBack = () => (
+        <div className="mt-5">
+            <Link to="/admin/dashboard" className="text-warning">
+                Back to Dashboard
+            </Link>
+        </div>
+    );
+
     return (
         <Layout
             title="Add a new category"
-            description={`G'day ${name}, ready to add a new category?`}
+            description={`G'day ${user.name}, ready to add a new category?`}
         >
             <div className="row">
-                <div className="col-md-8 offset-md-2">{newCategoryFom()}</div>
+                <div className="col-md-8 offset-md-2">
+                    {showSuccess()}
+                    {showError()}
+                    {newCategoryFom()}
+                    {goBack()}
+                </div>
             </div>
         </Layout>
     );
