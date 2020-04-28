4a3594e8900ca1c3dc922a7750df8ea898123ebe
Jaysonliu <leobest9688@gmail.com>
2020-04-28 21:15:37 +0800

    implementing of user purchase history in frontend

--- ecommerce-frontend/src/user/apiUser.js
+++ ecommerce-frontend/src/user/apiUser.js
@@ -41,3 +41,18 @@ export const updateUser = (user, next) => {
         }
     }
 };
+
+export const getPurchaseHistory = (userId, token) => {
+    return fetch(`${API}/orders/by/user/${userId}`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        }
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
