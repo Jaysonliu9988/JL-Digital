926d19c76d817e87d84f2ea842b8ca4649dfc865
Jaysonliu <leobest9688@gmail.com>
2020-04-28 20:42:46 +0800

    loop through orders

--- ecommerce-frontend/src/admin/Orders.js
+++ ecommerce-frontend/src/admin/Orders.js
@@ -3,6 +3,7 @@ import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
 import { listOrders } from "./apiAdmin";
+import moment from "moment";
 
 const Orders = () => {
     const [orders, setOrders] = useState([]);
@@ -23,8 +24,16 @@ const Orders = () => {
         loadOrders();
     }, []);
 
-    const noOrders = orders => {
-        return orders.length < 1 ? <h4>No orders</h4> : null;
+    const showOrdersLength = () => {
+        if (orders.length > 0) {
+            return (
+                <h1 className="text-danger display-2">
+                    Total orders: {orders.length}
+                </h1>
+            );
+        } else {
+            return <h1 className="text-danger">No orders</h1>;
+        }
     };
 
     return (
@@ -36,8 +45,50 @@ const Orders = () => {
         >
             <div className="row">
                 <div className="col-md-8 offset-md-2">
-                    {noOrders(orders)}
-                    {JSON.stringify(orders)}
+                    {showOrdersLength()}
+
+                    {orders.map((o, oIndex) => {
+                        return (
+                            <div
+                                className="mt-5"
+                                key={oIndex}
+                                style={{ borderBottom: "5px solid indigo" }}
+                            >
+                                <h2 className="mb-5">
+                                    <span className="bg-primary">
+                                        Order ID: {o._id}
+                                    </span>
+                                </h2>
+
+                                <ul className="list-group mb-2">
+                                    <li className="list-group-item">
+                                        {o.status}
+                                    </li>
+                                    <li className="list-group-item">
+                                        Transaction ID: {o.transaction_id}
+                                    </li>
+                                    <li className="list-group-item">
+                                        Amount: ${o.amount}
+                                    </li>
+                                    <li className="list-group-item">
+                                        Ordered by: {o.user.name}
+                                    </li>
+                                    <li className="list-group-item">
+                                        Ordered on:{" "}
+                                        {moment(o.createdAt).fromNow()}
+                                    </li>
+                                    <li className="list-group-item">
+                                        Delivery address: {o.address}
+                                    </li>
+                                </ul>
+
+                                <h3 className="mt-4 mb-4 font-italic">
+                                    Total products in the order:{" "}
+                                    {o.products.length}
+                                </h3>
+                            </div>
+                        );
+                    })}
                 </div>
             </div>
         </Layout>
