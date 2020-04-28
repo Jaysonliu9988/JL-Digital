318775cc3ad828dffecfee221f493f5fbe8f7560
Jaysonliu <leobest9688@gmail.com>
2020-04-28 20:52:32 +0800

    find order by id and update order status

--- ecommerce-backend/controllers/order.js
+++ ecommerce-backend/controllers/order.js
@@ -1,6 +1,20 @@
 const { Order, CartItem } = require("../models/order");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
+exports.orderById = (req, res, next, id) => {
+    Order.findById(id)
+        .populate("products.product", "name price")
+        .exec((err, order) => {
+            if (err || !order) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+            req.order = order;
+            next();
+        });
+};
+
 exports.create = (req, res) => {
     // console.log("CREATE ORDER: ", req.body);
     req.body.order.user = req.profile;
@@ -27,4 +41,23 @@ exports.listOrders = (req, res) => {
             }
             res.json(orders);
         });
+};
+
+exports.getStatusValues = (req, res) => {
+    res.json(Order.schema.path("status").enumValues);
+};
+
+exports.updateOrderStatus = (req, res) => {
+    Order.update(
+        { _id: req.body.orderId },
+        { $set: { status: req.body.status } },
+        (err, order) => {
+            if (err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+            res.json(order);
+        }
+    );
 };
--- ecommerce-backend/routes/order.js
+++ ecommerce-backend/routes/order.js
@@ -3,7 +3,13 @@ const router = express.Router();
 
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById, addOrderToUserHistory } = require("../controllers/user");
-const { create, listOrders, getStatusValues } = require("../controllers/order");
+const {
+    create,
+    listOrders,
+    getStatusValues,
+    orderById,
+    updateOrderStatus
+} = require("../controllers/order");
 const { decreaseQuantity } = require("../controllers/product");
 
 router.post(
@@ -23,7 +29,15 @@ router.get(
     isAdmin,
     getStatusValues
 );
+router.put(
+    "/order/:orderId/status/:userId",
+    requireSignin,
+    isAuth,
+    isAdmin,
+    updateOrderStatus
+);
 
 router.param("userId", userById);
+router.param("orderId", orderById);
 
 module.exports = router;
