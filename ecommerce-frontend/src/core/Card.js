5304ed5deaeb3afcd033c8b86a13f5efe1f0c599
Jaysonliu <leobest9688@gmail.com>
2020-04-28 17:19:04 +0800

    add to cart

--- ecommerce-frontend/src/core/Card.js
+++ ecommerce-frontend/src/core/Card.js
@@ -1,9 +1,12 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
 import ShowImage from "./ShowImage";
 import moment from "moment";
+import { addItem } from "./cartHelpers";
 
 const Card = ({ product, showViewProductButton = true }) => {
+    const [redirect, setRedirect] = useState(false);
+
     const showViewButton = showViewProductButton => {
         return (
             showViewProductButton && (
@@ -16,9 +19,24 @@ const Card = ({ product, showViewProductButton = true }) => {
         );
     };
 
+    const addToCart = () => {
+        addItem(product, () => {
+            setRedirect(true);
+        });
+    };
+
+    const shouldRedirect = redirect => {
+        if (redirect) {
+            return <Redirect to="/cart" />;
+        }
+    };
+
     const showAddToCartButton = () => {
         return (
-            <button className="btn btn-outline-warning mt-2 mb-2">
+            <button
+                onClick={addToCart}
+                className="btn btn-outline-warning mt-2 mb-2"
+            >
                 Add to cart
             </button>
         );
@@ -36,6 +54,7 @@ const Card = ({ product, showViewProductButton = true }) => {
         <div className="card">
             <div className="card-header name">{product.name}</div>
             <div className="card-body">
+                {shouldRedirect(redirect)}
                 <ShowImage item={product} url="product" />
                 <p className="lead mt-2">
                     {product.description.substring(0, 100)}
