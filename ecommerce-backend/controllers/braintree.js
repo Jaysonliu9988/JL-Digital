cfe345aa664aec9ab529a3e1c25af04060b98369
Jaysonliu <leobest9688@gmail.com>
2020-04-28 18:59:38 +0800

    processing payment backend

--- ecommerce-backend/controllers/braintree.js
+++ ecommerce-backend/controllers/braintree.js
@@ -18,3 +18,25 @@ exports.generateToken = (req, res) => {
         }
     });
 };
+
+exports.processPayment = (req, res) => {
+    let nonceFromTheClient = req.body.paymentMethodNonce;
+    let amountFromTheClient = req.body.amount;
+    // charge
+    let newTransaction = gateway.transaction.sale(
+        {
+            amount: amountFromTheClient,
+            paymentMethodNonce: nonceFromTheClient,
+            options: {
+                submitForSettlement: true
+            }
+        },
+        (error, result) => {
+            if (error) {
+                res.status(500).json(error);
+            } else {
+                res.json(result);
+            }
+        }
+    );
+};
