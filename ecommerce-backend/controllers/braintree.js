bba673586d9700b22f3ac245c3e56060fb995def
Jaysonliu <leobest9688@gmail.com>
2020-04-28 18:44:03 +0800

    braintree setup backend

---
+++ ecommerce-backend/controllers/braintree.js
@@ -0,0 +1,20 @@
+const User = require("../models/user");
+const braintree = require("braintree");
+require("dotenv").config();
+
+const gateway = braintree.connect({
+    environment: braintree.Environment.Sandbox,
+    merchantId: process.env.BRAINTREE_MERCHANT_ID,
+    publicKey: process.env.BRAINTREE_PUBLIC_KEY,
+    privateKey: process.env.BRAINTREE_PRIVATE_KEY
+});
+
+exports.generateToken = (req, res) => {
+    gateway.clientToken.generate({}, function(err, response) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.send(response);
+        }
+    });
+};
