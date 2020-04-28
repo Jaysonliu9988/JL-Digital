440224190191550af10216d95179982e2c0f4da6
Jaysonliu <leobest9688@gmail.com>
2020-04-28 20:49:59 +0800

    enum status values of each order

--- ecommerce-backend/routes/order.js
+++ ecommerce-backend/routes/order.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById, addOrderToUserHistory } = require("../controllers/user");
-const { create, listOrders } = require("../controllers/order");
+const { create, listOrders, getStatusValues } = require("../controllers/order");
 const { decreaseQuantity } = require("../controllers/product");
 
 router.post(
@@ -16,6 +16,13 @@ router.post(
 );
 
 router.get("/order/list/:userId", requireSignin, isAuth, isAdmin, listOrders);
+router.get(
+    "/order/status-values/:userId",
+    requireSignin,
+    isAuth,
+    isAdmin,
+    getStatusValues
+);
 
 router.param("userId", userById);
 
