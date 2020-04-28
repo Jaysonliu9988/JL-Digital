ce1830846697b467905ab9d56afb4af7fec43ea3
Jaysonliu <leobest9688@gmail.com>
2020-04-28 20:34:59 +0800

    list all orders for admin

--- ecommerce-backend/controllers/order.js
+++ ecommerce-backend/controllers/order.js
@@ -14,3 +14,17 @@ exports.create = (req, res) => {
         res.json(data);
     });
 };
+
+exports.listOrders = (req, res) => {
+    Order.find()
+        .populate("user", "_id, name, address")
+        .sort("-created")
+        .exec((err, orders) => {
+            if (err) {
+                return res.status(400).json({
+                    error: errorHandler(error)
+                });
+            }
+            res.json(orders);
+        });
+};
--- ecommerce-backend/routes/order.js
+++ ecommerce-backend/routes/order.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { requireSignin, isAuth } = require("../controllers/auth");
+const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById, addOrderToUserHistory } = require("../controllers/user");
-const { create } = require("../controllers/order");
+const { create, listOrders } = require("../controllers/order");
 const { decreaseQuantity } = require("../controllers/product");
 
 router.post(
@@ -14,6 +14,8 @@ router.post(
     decreaseQuantity,
     create
 );
+
+router.get("/order/list/:userId", requireSignin, isAuth, isAdmin, listOrders);
 
 router.param("userId", userById);
 
