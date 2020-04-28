3d95e4f3cbd051ef8518ff313a9cd179667098e6
Jaysonliu <leobest9688@gmail.com>
2020-04-28 19:02:52 +0800

    make successful transaction

--- ecommerce-frontend/src/core/Checkout.js
+++ ecommerce-frontend/src/core/Checkout.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { getProducts, getBraintreeClientToken } from "./apiCore";
+import {
+    getProducts,
+    getBraintreeClientToken,
+    processPayment
+} from "./apiCore";
 import Card from "./Card";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
@@ -23,7 +27,7 @@ const Checkout = ({ products }) => {
             if (data.error) {
                 setData({ ...data, error: data.error });
             } else {
-                setData({ ...data, clientToken: data.clientToken });
+                setData({ clientToken: data.clientToken });
             }
         });
     };
@@ -55,18 +59,31 @@ const Checkout = ({ products }) => {
         let getNonce = data.instance
             .requestPaymentMethod()
             .then(data => {
-                console.log(data);
+                // console.log(data);
                 nonce = data.nonce;
                 // once you have nonce (card type, card number) send nonce as 'paymentMethodNonce'
                 // and also total to be charged
-                console.log(
-                    "send nonce and total to process: ",
-                    nonce,
-                    getTotal(products)
-                );
+                // console.log(
+                //     "send nonce and total to process: ",
+                //     nonce,
+                //     getTotal(products)
+                // );
+                const paymentData = {
+                    paymentMethodNonce: nonce,
+                    amount: getTotal(products)
+                };
+
+                processPayment(userId, token, paymentData)
+                    .then(response => {
+                        // console.log(response
+                        setData({ ...data, success: response.success });
+                        // empty cart
+                        // create order
+                    })
+                    .catch(error => console.log(error));
             })
             .catch(error => {
-                console.log("dropin error: ", error);
+                // console.log("dropin error: ", error);
                 setData({ ...data, error: error.message });
             });
     };
@@ -98,9 +115,19 @@ const Checkout = ({ products }) => {
         </div>
     );
 
+    const showSuccess = success => (
+        <div
+            className="alert alert-info"
+            style={{ display: success ? "" : "none" }}
+        >
+            Thanks! Your payment was successful!
+        </div>
+    );
+
     return (
         <div>
             <h2>Total: ${getTotal()}</h2>
+            {showSuccess(data.success)}
             {showError(data.error)}
             {showCheckout()}
         </div>
--- ecommerce-frontend/src/core/apiCore.js
+++ ecommerce-frontend/src/core/apiCore.js
@@ -89,3 +89,19 @@ export const getBraintreeClientToken = (userId, token) => {
         })
         .catch(err => console.log(err));
 };
+
+export const processPayment = (userId, token, paymentData) => {
+    return fetch(`${API}/braintree/payment/${userId}`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(paymentData)
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
